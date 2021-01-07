720913b8b90b8ec93774cb7c5bc36c865f44203f
opiumpopp <524185788@qq.com>
2021-01-07 21:29:56 +0800

    P125：Sentinel热点key(下)。

--- cloud-alibaba-sentinel-service8401/src/main/java/com/atguigu/springcoud/alibaba/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/atguigu/springcoud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.atguigu.springcoud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.sun.deploy.security.BlockedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -42,6 +45,17 @@ public class FlowLimitController {
         log.info("testE 测试异常数");
         int age = 10 / 0;
         return "------testE 测试异常数";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                              @RequestParam(value = "p2", required = false) String p2) {
+        return "------testHotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockedException exception) {
+        return "------deal_testHotKey, (￣▽￣)";
     }
 
 }

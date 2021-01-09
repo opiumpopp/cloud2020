0b153c0945143379f37c4a077b2b830c17c3cb02
opiumpopp <524185788@qq.com>
2021-01-09 21:32:10 +0800

    P130：Sentinel服务熔断Ribbon环境预说。

---
+++ cloud-alibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/alibaba/config/ApplicationContextConfig.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.alibaba.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author opiumpopp
+ * @create 2021-01-09
+ */
+@Configuration
+public class ApplicationContextConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+}
